9a874e806276f4b139978fad6bbc476ee0fe75b3
Jithin Emmanuel <jithin1987@gmail.com>
2017-09-18 21:23:32 -0700

    init

---
+++ jspm.config.js
@@ -0,0 +1,52 @@
+SystemJS.config({
+  browserConfig: {
+    "baseURL": ".",
+    "paths": {
+      "npm:": "/jspm_packages/npm/",
+      "app/": "/src/"
+    }
+  },
+  nodeConfig: {
+    "paths": {
+      "npm:": "jspm_packages/npm/",
+      "app/": "src/"
+    }
+  },
+  devConfig: {
+    "map": {
+      "plugin-babel": "npm:systemjs-plugin-babel@0.0.25",
+      "babel-runtime": "npm:babel-runtime@5.8.38",
+      "core-js": "npm:core-js@1.2.7",
+      "babel": "npm:babel-core@5.8.38",
+      "process": "npm:jspm-nodelibs-process@0.2.1",
+      "fs": "npm:jspm-nodelibs-fs@0.2.1",
+      "path": "npm:jspm-nodelibs-path@0.2.3"
+    }
+  },
+  transpiler: "plugin-babel",
+  packages: {
+    "app": {
+      "main": "app.js",
+      "meta": {
+        "app/cjs.js": {
+          "format": "cjs"
+        },
+        "*.js": {
+          "loader": "plugin-babel"
+        }
+      }
+    }
+  }
+});
+
+SystemJS.config({
+  "browserConfig": {
+    "baseURL": "."
+  },
+  packageConfigPaths: [
+    "npm:@*/*.json",
+    "npm:*.json"
+  ],
+  map: {},
+  packages: {}
+});
---
+++ src/app.js
@@ -0,0 +1,7 @@
+import ES6 from './es6.js';
+import CJS from './cjs.js';
+
+let container = document.getElementById('container');
+
+console.log('ES6', ES6);
+console.log('CJS', CJS);
